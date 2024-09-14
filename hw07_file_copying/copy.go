503a1cfe16f90f91cf80210b3aa25ab880fcc96b
azimkhan <wanrossen@gmail.com>
2024-09-14 19:06:31 +0500

    HW7 done

--- hw07_file_copying/copy.go
+++ hw07_file_copying/copy.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"io"
+	"os"
 )
 
 var (
@@ -9,7 +12,87 @@ var (
 	ErrOffsetExceedsFileSize = errors.New("offset exceeds file size")
 )
 
-func Copy(fromPath, toPath string, offset, limit int64) error {
-	// Place your code here.
+const bufferSize = 1024 * 1024
+
+func copyInternal(dest io.Writer, source io.Reader, bytesToCopy int64) error {
+	// copy source to dest while printing progress
+	// return error if any
+	buff := make([]byte, bufferSize)
+	offset := 0
+
+	printProgress := func() {
+		percentage := float64(offset) / float64(bytesToCopy) * 100
+		fmt.Printf("\rCopy progress: %.2f%%", percentage)
+	}
+
+	for {
+		n, err := source.Read(buff)
+		offset += n
+		if err != nil {
+			if errors.Is(err, io.EOF) {
+				break
+			}
+			return err
+		}
+		_, err = dest.Write(buff[:n])
+		if err != nil {
+			return err
+		}
+		printProgress()
+	}
+	printProgress()
+	println()
 	return nil
+}
+
+func Copy(fromPath, toPath string, offset, limit int64) error {
+	var source *os.File
+	var dest *os.File
+	var err error
+	var reader io.Reader
+
+	// check if file has size
+	fs, err := os.Stat(fromPath)
+	if err != nil {
+		return err
+	}
+	if fs.Size() == 0 {
+		return ErrUnsupportedFile
+	}
+
+	// Open source file
+	source, err = os.Open(fromPath)
+	if err != nil {
+		return err
+	}
+	defer source.Close()
+
+	if offset > 0 {
+		if fs.Size() < offset {
+			return ErrOffsetExceedsFileSize
+		}
+		_, err = source.Seek(offset, io.SeekStart)
+		if err != nil {
+			return err
+		}
+	}
+
+	// Open destination file
+	dest, err = os.Create(toPath)
+	if err != nil {
+		return err
+	}
+	defer dest.Close()
+
+	if limit > 0 {
+		reader = io.LimitReader(source, limit)
+	} else {
+		reader = source
+	}
+
+	bytesToCopy := fs.Size() - offset
+	if limit > 0 && (limit < bytesToCopy) {
+		bytesToCopy = limit
+	}
+	return copyInternal(dest, reader, bytesToCopy)
 }
